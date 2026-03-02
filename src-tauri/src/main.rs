8b6de089e4c1a44645682511b4508b1493796b27
rayandu924 <rayandu924@gmail.com>
2026-03-02 16:20:24 +0100

    fix: hide console window in debug builds (logs still in file)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,5 @@
-// Prevents additional console window on Windows in release
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+// Prevents additional console window on Windows (debug & release)
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 fn main() {
     mywallpaper_desktop_lib::main();
